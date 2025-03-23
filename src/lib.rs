4279579f1d1855d510834572f01a28df66098f92
tb <travisbikkle@proton.me>
2025-03-23 22:58:45 +0800

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,16 @@ pub mod tui;
 pub mod constants;
 pub use constants::*;
 
+pub fn load_config(config_path: &str) -> config::Config {
+    let mut file = File::open(config_path).expect("Failed to open config file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to read config file");
+
+    let config: config::Config = serde_yaml::from_str(&contents).expect("Failed to parse config file");
+    config
+}
+
 pub fn init_loop() {
     let in_band = false;
     if in_band {
@@ -19,12 +29,7 @@ pub fn init_loop() {
     }
 
     let config_path = format!("{}/HR650X.yaml", std::env::current_dir().unwrap().display());
-    let mut file = File::open(config_path).expect("Failed to open config file");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Failed to read config file");
-
-    let config: config::Config = serde_yaml::from_str(&contents).expect("Failed to parse config file");
+    let config: config::Config = load_config(&config_path);
 
     let ipmi_tool_cmd = if in_band {
         "ipmitool".to_string()
@@ -39,11 +44,26 @@ pub fn init_loop() {
 
     loop {
         if let Some((temp, cpu_num)) = fan::get_temperature_and_cpu_num(&ipmi_tool_cmd) {
+            // show on tui monitor
             let speed = fan::get_fan_speed(temp, &config.fan_speeds);
             if fan::set_fan_speed(speed, &ipmi_tool_cmd, cpu_num, &mut cpu2_fan_speed_set) {
+                // show log on tui
                 println!("Set fan speed to {}% for CPU temperature {}°C", speed, temp);
             }
         }
+        // tokio async
         thread::sleep(Duration::from_secs(10));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_config() {
+        let config_path = format!("{}/{}/HR650X.yaml", std::env::current_dir().unwrap().display(), "scripts");
+        let config = load_config(&config_path);
+        assert_eq!(config.ipmi.username, "changeme");
     }
 }
