1b58d8390e91884419bbd0f6f940233593261d24
travisbikkle <zhaoyu025@gmail.com>
2025-03-25 01:03:59 +0800

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,7 @@ cp target/x86_64-unknown-linux-musl/release/smartfan dist/linux/
 cp scripts/config.yaml dist/linux/
 cp scripts/install.sh dist/linux/
 cp scripts/*.txt dist/linux/
+cp LICENSE.txt dist/linux/
 cd dist/linux
 zip -r ../smartfan-linux.zip .
 cd ../..
@@ -19,6 +20,7 @@ mkdir -p dist/windows
 cp target/x86_64-pc-windows-gnu/release/smartfan.exe dist/windows/
 cp scripts/config.yaml dist/windows/
 cp scripts/*.txt dist/windows/
+cp LICENSE.txt dist/windows/
 cd dist/windows
 zip -r ../smartfan-windows.zip .
 cd ../..
